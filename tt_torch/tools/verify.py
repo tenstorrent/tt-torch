4f5c2d2e70da6a1d536ec0b92ccdf79d55b2df1a
Aleks Knezevic <aknezevic@tenstorrent.com>
2024-10-21 14:31:11 +0000

    Added verify file

---
+++ tt_torch/tools/verify.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+from tt_torch.dynamo.backend import backend
+
+def verify_module(mod, input_shapes, input_data_types=[torch.float32],required_pcc=0.99, required_atol=1e-2):
+    tt_mod = torch.compile(mod, backend=backend)
+
+    if all([dtype.is_floating_point for dtype in input_data_types]):
+      inputs = [torch.rand(shape)-0.5 for shape in input_shapes]
+    else:
+      inputs = [torch.randint(0, 1000, shape) for shape in input_shapes]
+    ret = tt_mod(*inputs)
+    golden = mod(*inputs)
+
+    atol = torch.max(torch.abs(golden - ret)).item()
+    assert atol <= required_atol, f"ATOL too high: {atol} vs {required_atol}"
+    pcc = np.min(
+        np.ma.corrcoef(
+            np.ma.masked_invalid(torch.squeeze(ret).detach().numpy()).flatten(),
+            np.ma.masked_invalid(torch.squeeze(golden).detach().numpy()).flatten(),
+        )
+    )
+    assert pcc >= required_pcc, f"PCC too low: {pcc} vs {required_pcc}"
