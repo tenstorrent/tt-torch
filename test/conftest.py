9645072900e3c697962e88f4f1dd57255d97c306
Lewis Panos <lpanos@tenstorrent.com>
2024-10-28 21:37:13 +0000

    Add slice tests, fix module compilation

---
+++ test/conftest.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    yield
+    torch._dynamo.reset()
--- test/test_basic.py
+++ test/test_basic.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import pytest
 
+import tt_torch
 from tt_torch.tools.verify import verify_module
 
 def test_add():
@@ -115,6 +116,54 @@ def test_sqrt():
       return torch.sqrt(x)
     
   verify_module(Basic(), [(32, 32)], required_atol=3e-2, input_range=(0.1, 1))
+
+dim0_cases = []
+for begin in torch.arange(10).tolist():
+  for end in torch.arange(90, 100).tolist():
+    dim0_cases.append((begin, end, 0))
+
+dim1_cases = []
+for begin in torch.arange(10).tolist():
+  for end in torch.arange(90, 100).tolist():
+    dim1_cases.append((begin, end, 1))
+
+dim2_cases = [] 
+for begin in torch.arange(0, 64, 32).tolist():
+  for end in torch.arange(64, 128, 32).tolist():
+    dim2_cases.append((begin, end, 2))
+
+dim3_cases = []
+for begin in torch.arange(0, 64, 32).tolist():
+  for end in torch.arange(64, 128, 32).tolist():
+    dim3_cases.append((begin, end, 3))
+
+@pytest.mark.parametrize(
+  "begin, end, dim",
+  [
+    *dim2_cases,
+    *dim3_cases,
+    *dim0_cases,
+    *dim1_cases
+  ]
+)
+def test_slice(begin, end, dim):
+  class Basic(nn.Module):
+    def __init__(self):
+      super().__init__()
+
+    def forward(self, a):
+      if dim == 0:
+        return a[begin:end, :, :, :]
+      elif dim == 1:
+        return a[:, begin:end, :, :]
+      elif dim == 2:
+        return a[:, :, begin:end, :]
+      else:
+        return a[:, :, :, begin:end]
+
+  shape = [10, 10, 10, 10]
+  shape[dim] = 128
+  verify_module(Basic(), [shape])
 
 def test_bert():
   from torch_mlir import fx
