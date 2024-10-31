6e5bd7bae0384b78849a5f4af4a3a8255d941986
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2024-10-31 11:24:43 -0400

    Fix formatting issues (#9)
    * Added required files for pre-commit formatting.
    * Applied Tenstorrent formatting style.

--- tt_torch/csrc/tt-mlir-interface.hpp
+++ tt_torch/csrc/tt-mlir-interface.hpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #pragma once
 #include <cstdlib>
 #include <iostream>
@@ -5,5 +9,5 @@
 #include "tt/runtime/runtime.h"
 
 namespace tt::torch {
-  tt::runtime::Binary Compile(std::string_view code);
-}  // namespace tt::torch
+tt::runtime::Binary Compile(std::string_view code);
+} // namespace tt::torch
--- tt_torch/tools/verify.py
+++ tt_torch/tools/verify.py
@@ -1,15 +1,28 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
 import torch
 import numpy as np
 from tt_torch.dynamo.backend import backend
 
-def verify_module(mod, input_shapes, input_data_types=[torch.float32],required_pcc=0.99, required_atol=1e-2, input_range=(-0.5, 0.5)):
+
+def verify_module(
+    mod,
+    input_shapes,
+    input_data_types=[torch.float32],
+    required_pcc=0.99,
+    required_atol=1e-2,
+    input_range=(-0.5, 0.5),
+):
     tt_mod = torch.compile(mod, backend=backend)
 
     if all([dtype.is_floating_point for dtype in input_data_types]):
-      low, high = input_range 
-      inputs = [(low - high) * torch.rand(shape) + high for shape in input_shapes] # uniformly distribute random numbers within the input_range
+        low, high = input_range
+        inputs = [
+            (low - high) * torch.rand(shape) + high for shape in input_shapes
+        ]  # uniformly distribute random numbers within the input_range
     else:
-      inputs = [torch.randint(0, 1000, shape) for shape in input_shapes]
+        inputs = [torch.randint(0, 1000, shape) for shape in input_shapes]
     ret = tt_mod(*inputs)
     golden = mod(*inputs)
 
