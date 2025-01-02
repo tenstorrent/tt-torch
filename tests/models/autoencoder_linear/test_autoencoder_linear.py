8bb82fb41905f2e1088c50a6566b9fecd5cc7a78
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2025-01-02 19:34:27 +0000

    Disable running autoencoder_linear model test as E2E test with CI

--- tests/models/autoencoder_linear/test_autoencoder_linear.py
+++ tests/models/autoencoder_linear/test_autoencoder_linear.py
@@ -96,6 +96,8 @@ def test_autoencoder_linear(record_property, mode, nightly):
     cc.consteval_parameters = True
     if nightly:
         cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
+    else:
+        cc.compile_depth = CompileDepth.TTNN_IR
 
     tester = ThisTester(model_name, mode, compiler_config=cc)
     results = tester.test_model()
