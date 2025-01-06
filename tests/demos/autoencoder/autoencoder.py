8d67ac74a37792a84d07dda617b4e40b90ae3ec5
Tapasvi Patel <tpatel@tenstorrent.com>
2025-01-06 23:45:06 +0000

    wip

--- tests/demos/autoencoder/autoencoder.py
+++ tests/demos/autoencoder/autoencoder.py
@@ -8,96 +8,6 @@ import torchvision.transforms as transforms
 from tt_torch.tools.verify import verify_module
 from tt_torch.tools.utils import CompilerConfig, CompileDepth
 
-class EncoderLinearOne(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.encoder_lin1 = torch.nn.Linear(784, 128)
-
-    def forward(self, x):
-        act = self.encoder_lin1(x)
-        return act
-
-class EncoderLinearTwo(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.encoder_lin1 = torch.nn.Linear(128, 64)
-
-    def forward(self, x):
-        act = self.encoder_lin1(x)
-        return act
-
-class EncoderLinearThree(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.encoder_lin1 = torch.nn.Linear(64, 12)
-
-    def forward(self, x):
-        act = self.encoder_lin1(x)
-        return act
-
-class EncoderLinearFour(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.encoder_lin1 = torch.nn.Linear(12, 3)
-
-    def forward(self, x):
-        act = self.encoder_lin1(x)
-        return act
-
-class DecoderLinearOne(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.decoder_lin1 = torch.nn.Linear(3, 12)
-
-    def forward(self, x):
-        act = self.decoder_lin1(x)
-        return act
-
-class DecoderLinearTwo(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.decoder_lin1 = torch.nn.Linear(12, 64)
-
-    def forward(self, x):
-        act = self.decoder_lin1(x)
-        return act
-
-class DecoderLinearThree(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.decoder_lin1 = torch.nn.Linear(64, 128)
-
-    def forward(self, x):
-        act = self.decoder_lin1(x)
-        return act
-
-class DecoderLinearFour(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.decoder_lin1 = torch.nn.Linear(128, 784)
-
-    def forward(self, x):
-        act = self.decoder_lin1(x)
-        return act
-
-class Relu(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.act_fun = torch.nn.ReLU()
-
-    def forward(self, x):
-        act = self.act_fun(x)
-        return act
-
 class LinearAE(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -139,6 +49,16 @@ class LinearAE(torch.nn.Module):
         return act
 
 def test_encoder_linear_one():
+    class EncoderLinearOne(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.encoder_lin1 = torch.nn.Linear(784, 128)
+
+        def forward(self, x):
+            act = self.encoder_lin1(x)
+            return act
+
     cc = CompilerConfig()
     cc.enable_consteval = True
     cc.consteval_parameters = True
@@ -147,27 +67,107 @@ def test_encoder_linear_one():
     verify_module(EncoderLinearOne(), input_shapes=[(1, 784)], compiler_config=cc)
 
 def test_encoder_linear_two():
+    class EncoderLinearTwo(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.encoder_lin1 = torch.nn.Linear(128, 64)
+
+        def forward(self, x):
+            act = self.encoder_lin1(x)
+            return act
+
     verify_module(EncoderLinearTwo(), input_shapes=[(1, 128)])
 
 def test_encoder_linear_three():
+    class EncoderLinearThree(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.encoder_lin1 = torch.nn.Linear(64, 12)
+
+        def forward(self, x):
+            act = self.encoder_lin1(x)
+            return act
+
     verify_module(EncoderLinearThree(), input_shapes=[(1, 64)])
 
 def test_encoder_linear_four():
+    class EncoderLinearFour(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.encoder_lin1 = torch.nn.Linear(12, 3)
+
+        def forward(self, x):
+            act = self.encoder_lin1(x)
+            return act
+
     verify_module(EncoderLinearFour(), input_shapes=[(1, 12)])
 
 def test_decoder_linear_one():
+    class DecoderLinearOne(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.decoder_lin1 = torch.nn.Linear(3, 12)
+
+        def forward(self, x):
+            act = self.decoder_lin1(x)
+            return act
+
     verify_module(DecoderLinearOne(), input_shapes=[(1, 3)])
 
 def test_decoder_linear_two():
+    class DecoderLinearTwo(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.decoder_lin1 = torch.nn.Linear(12, 64)
+
+        def forward(self, x):
+            act = self.decoder_lin1(x)
+            return act
+
     verify_module(DecoderLinearTwo(), input_shapes=[(1, 12)])
 
 def test_decoder_linear_three():
+    class DecoderLinearThree(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.decoder_lin1 = torch.nn.Linear(64, 128)
+
+        def forward(self, x):
+            act = self.decoder_lin1(x)
+            return act
+
     verify_module(DecoderLinearThree(), input_shapes=[(1, 64)])
 
 def test_decoder_linear_four():
+    class DecoderLinearFour(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.decoder_lin1 = torch.nn.Linear(128, 784)
+
+        def forward(self, x):
+            act = self.decoder_lin1(x)
+            return act
+
     verify_module(DecoderLinearFour(), input_shapes=[(1, 128)])
 
 def test_relu():
+    class Relu(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+            self.act_fun = torch.nn.ReLU()
+
+        def forward(self, x):
+            act = self.act_fun(x)
+            return act
+
     verify_module(Relu(), input_shapes=[(1, 12)])
     verify_module(Relu(), input_shapes=[(1, 64)])
     verify_module(Relu(), input_shapes=[(1, 128)])
