48bcb2f92737dab6bc9b7b7aea945f0c7ca4bcdb
uazizTT <uaziz@tenstorrent.com>
2025-01-24 10:22:39 -0500

    Mark some tests as passing.

--- tests/torch/test_basic.py
+++ tests/torch/test_basic.py
@@ -185,10 +185,6 @@ def test_linear():
     verify_module(Basic(), input_shapes=[(32, 32)])
 
 
-@pytest.mark.xfail(
-    strict=True,
-    reason="Embedded constants currently broken, see https://github.com/tenstorrent/tt-torch/issues/152",
-)
 def test_linear_with_bias():
     class Basic(nn.Module):
         def __init__(self):
@@ -204,7 +200,7 @@ def test_linear_with_bias():
 
 @pytest.mark.xfail(
     strict=True,
-    reason="RepeatOp is failing due to scalar type shape mismatch, see https://github.com/tenstorrent/tt-torch/issues/186",
+    reason="PCC and ATOL mismatch",
 )
 def test_linear_with_bias_no_embedded_constants():
     class Basic(nn.Module):
