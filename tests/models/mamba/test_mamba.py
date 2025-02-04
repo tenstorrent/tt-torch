f05a28fb52eb8dde34def342f48a64354b598e29
Defne Dilbaz <ddilbaz@tenstorrent.com>
2025-02-04 20:00:31 +0000

    Fix mamba test arguments

--- tests/models/mamba/test_mamba.py
+++ tests/models/mamba/test_mamba.py
@@ -64,9 +64,7 @@ def test_mamba(record_property, model_name, mode, op_by_op):
     if op_by_op:
         cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
 
-    tester = ThisTester(
-        model_name, mode, assert_on_output_mismatch=False, compiler_config=cc
-    )
+    tester = ThisTester(model_name, mode, compiler_config=cc)
     results = tester.test_model()
 
     if mode == "eval":
