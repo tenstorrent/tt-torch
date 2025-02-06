e80d4c23e9c01a545fef32bf97caab3ca35cd695
Defne Dilbaz <ddilbaz@tenstorrent.com>
2025-02-06 22:13:54 +0000

    Add llama 3b [#289]
    Add meta-llama/Llama-3.2-3B from https://huggingface.co/meta-llama/Llama-3.2-3B

---
+++ tests/models/llama/test_llama_3b.py
@@ -0,0 +1,56 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import torch
+import pytest
+from tests.utils import ModelTester
+from tt_torch.tools.utils import CompilerConfig, CompileDepth
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+
+
+class ThisTester(ModelTester):
+    def _load_model(self):
+        model = AutoModelForCausalLM.from_pretrained(
+            self.model_name, torch_dtype=torch.bfloat16
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name, torch_dtype=torch.bfloat16
+        )
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        return model
+
+    def _load_inputs(self):
+        self.test_input = "This is a sample text from "
+        inputs = self.tokenizer.encode_plus(
+            self.test_input,
+            return_tensors="pt",
+            max_length=32,
+            padding="max_length",
+            truncation=True,
+        )
+        return inputs
+
+    def set_model_eval(self, model):
+        return model
+
+
+@pytest.mark.parametrize(
+    "mode",
+    ["eval"],
+)
+@pytest.mark.parametrize("model_name", ["meta-llama/Llama-3.2-3B"])
+@pytest.mark.parametrize("op_by_op", [True, False], ids=["op_by_op", "full"])
+def test_llama_3b(record_property, model_name, mode, op_by_op):
+    record_property("model_name", model_name)
+    record_property("mode", mode)
+
+    cc = CompilerConfig()
+    if op_by_op:
+        cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
+
+    tester = ThisTester(
+        model_name, mode, compiler_config=cc, assert_atol=False, assert_pcc=False
+    )
+    results = tester.test_model()
+
+    record_property("torch_ttnn", (tester, results))
