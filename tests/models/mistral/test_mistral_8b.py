6d0a1f26d96b773853ac09dbffdcff48d7ef46c2
Jonathan Azpur <jazpur@tenstorrent.com>
2025-03-01 04:25:54 +0000

    Add tests for mistral 8B and 12B models #263

---
+++ tests/models/mistral/test_mistral_8b.py
@@ -0,0 +1,68 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import torch
+import pytest
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from tests.utils import ModelTester
+from tt_torch.tools.utils import CompilerConfig, CompileDepth
+
+class ThisTester(ModelTester):
+    def _load_model(self):
+        # Download model from cloud
+        model_name = "mistralai/Ministral-8B-Instruct-2410"
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name, padding_side="left", torch_dtype=torch.bfloat16 
+        )
+        m = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+        return m
+
+    def _load_inputs(self):
+        # Set up sample input
+        self.test_input = "This is a sample text from "
+        inputs = self.tokenizer.encode_plus(self.test_input, return_tensors="pt")
+        return inputs
+
+
+@pytest.mark.parametrize(
+    "mode",
+    ["eval"],
+)
+@pytest.mark.xfail(
+    reason="mistral-8b is too large to fit on single device"
+)
+@pytest.mark.parametrize("op_by_op", [True, False], ids=["op-by-op","full"])
+def test_mistral_8b(record_property, mode, op_by_op):
+    model_name = "Mistral-8B"
+
+    cc = CompilerConfig()
+    if op_by_op:
+        cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
+
+    tester = ThisTester(
+        model_name,
+        mode,
+        compiler_config=cc,
+        record_property_handle=record_property,
+    )
+    results = tester.test_model()
+    if mode == "eval":
+        def decode_output(outputs):
+            # Assuming outputs.logits contains the logits
+            next_token_logits = outputs.logits[:, -1]
+            next_token = next_token_logits.softmax(dim=-1).argmax()
+            return tester.tokenizer.decode([next_token])
+
+        decoded_output = decode_output(results)
+
+        print(
+            f"""
+        model_name: {model_name}
+        input: {tester.test_input}
+        output before: {decoded_output}
+        """
+        )
+
+    tester.finalize()
