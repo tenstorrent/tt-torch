28f1ad7e44afcbe79b9c58c4b2f9c722065ad447
Lewis Panos <lpanos@tenstorrent.com>
2025-03-03 22:13:12 +0000

    Added OnnxModelTester. Added onnx mobilenetv2 test
    Updates to verify_against_golden messages.

--- tt_torch/onnx_compile/onnx_compile.py
+++ tt_torch/onnx_compile/onnx_compile.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import onnx
+import torch
+import onnxruntime as ort
 from torch_mlir.extras import onnx_importer
 import tt_mlir
 from torch_mlir.ir import Context
@@ -9,11 +11,15 @@ from torch_mlir.dialects import torch as torch_dialect
 import os
 import sys
 
+from tt_torch.tools.utils import CompilerConfig, CompileDepth
+
 from torch_mlir.compiler_utils import (
     OutputType,
     run_pipeline_with_repro_report,
     lower_mlir_module,
 )
+
+from torch_mlir.ir import Module
 
 from tt_torch.tools.utils import (
     OpByOpBackend,
@@ -22,14 +28,11 @@ from tt_torch.tools.utils import (
 )
 
 from tt_torch.dynamo.shlo_backend import StablehloExecutor
-from tt_torch.dynamo.executor import Executor
-from tt_torch.dynamo.backend import dump_module
+from tt_torch.dynamo.executor import OnnxExecutor
+from tt_torch.dynamo.backend import dump_module, shlo_to_flatbuffer
 
 
-def onnx_to_stablehlo(module: onnx.ModelProto, compiler_config):
-    # Infer onnx shapes incase that information is missing
-    module = onnx.shape_inference.infer_shapes(module)
-
+def generate_torch_onnx_ir(module: onnx.ModelProto, compiler_config: CompilerConfig):
     context = Context()
     torch_dialect.register_dialect(context)
     module_info = onnx_importer.ModelInfo(module)
@@ -37,8 +40,13 @@ def onnx_to_stablehlo(module: onnx.ModelProto, compiler_config):
     imp = onnx_importer.NodeImporter.define_function(module_info.main_graph, module)
     imp.import_all()
 
-    dump_module(module=module, name="ONNX module", compiler_config=compiler_config)
+    dump_module(
+        module=module, name="Torch Onnx module", compiler_config=compiler_config
+    )
+    return module
 
+
+def torch_onnx_to_torch_backend_ir(module: Module, compiler_config: CompilerConfig):
     run_pipeline_with_repro_report(
         module,
         "builtin.module(torch-onnx-to-torch-backend-pipeline)",
@@ -46,37 +54,55 @@ def onnx_to_stablehlo(module: onnx.ModelProto, compiler_config):
     )
 
     dump_module(
-        module=module, name="Torch Backend module", compiler_config=compiler_config
+        module=module,
+        name="Torch Backend module",
+        compiler_config=compiler_config,
     )
+    return module
 
+
+def torch_backend_ir_to_stablehlo(module: Module, compiler_config: CompilerConfig):
     lower_mlir_module(False, OutputType.STABLEHLO, module)
-
     dump_module(module=module, name="StableHLO module", compiler_config=compiler_config)
     return module
 
 
-def compile_onnx(module: onnx.ModelProto, example_inputs, options=None):
+def compile_onnx(model_proto: onnx.ModelProto, compiler_config: CompilerConfig = None):
+    if compiler_config is None:
+        compiler_config = CompilerConfig()
 
-    if options is None:
-        options = CompilerConfig()
-    options.op_by_op_backend = OpByOpBackend.STABLEHLO
-    options.typecast_inputs = False
-    options.apply_environment_overrides()
+    assert isinstance(
+        compiler_config, CompilerConfig
+    ), "compiler_config must be an instance of CompilerConfig"
+
+    compiler_config.op_by_op_backend = OpByOpBackend.STABLEHLO
+    compiler_config.typecast_inputs = False
+    compiler_config.apply_environment_overrides()
     if (
-        options.compile_depth == CompileDepth.COMPILE_OP_BY_OP
-        or options.compile_depth == CompileDepth.EXECUTE_OP_BY_OP
+        compiler_config.compile_depth == CompileDepth.COMPILE_OP_BY_OP
+        or compiler_config.compile_depth == CompileDepth.EXECUTE_OP_BY_OP
     ):
-        module = onnx_to_stablehlo(module, options)
-        executor = StablehloExecutor(module=module, compiler_config=options)
-        return executor(*example_inputs)
-    elif options.compile_depth == CompileDepth.EXECUTE:
-        module = onnx_to_stablehlo(module, options)
-        executor = Executor(gm=None, graph_constants=None, compiler_config=options)
-        ttir = tt_mlir.compile_stable_hlo_to_ttir(
-            module.operation.get_asm(enable_debug_info=True)
-        )
-        dump_module(module=ttir, name="TTIR module", compiler_config=options)
-        binary, ttnn = tt_mlir.compile_ttir_to_bytestream(ttir)
-        dump_module(module=ttnn, name="TTNN module", compiler_config=options)
+        model_proto = onnx.shape_inference.infer_shapes(model_proto)
+        module = generate_torch_onnx_ir(model_proto, compiler_config)
+        module = torch_onnx_to_torch_backend_ir(module, compiler_config)
+        module = torch_backend_ir_to_stablehlo(module, compiler_config)
+        executor = StablehloExecutor(module=module, compiler_config=compiler_config)
+        return executor
+    else:
+        model_proto = onnx.shape_inference.infer_shapes(model_proto)
+        executor = OnnxExecutor(model_proto)
+
+        module = generate_torch_onnx_ir(model_proto, compiler_config)
+
+        module = torch_onnx_to_torch_backend_ir(module, compiler_config)
+        if compiler_config.profile_ops:
+            compiler_config.set_torch_mlir_module(module.operation.get_asm())
+
+        module = torch_backend_ir_to_stablehlo(module, compiler_config)
+        if compiler_config.profile_ops:
+            compiler_config.set_stablehlo_mlir_module(module.operation.get_asm())
+        if compiler_config.compile_depth == CompileDepth.STABLEHLO:
+            return executor
+        binary = shlo_to_flatbuffer(executor, module, compiler_config)
         executor.set_binary(binary)
-        return executor(*example_inputs)
+        return executor
