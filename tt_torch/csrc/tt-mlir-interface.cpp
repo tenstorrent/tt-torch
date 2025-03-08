bc470e70ff6e4d5636825db45c46f01afde3af83
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-03-08 16:36:53 +0000

    rdv

--- tt_torch/csrc/tt-mlir-interface.cpp
+++ tt_torch/csrc/tt-mlir-interface.cpp
@@ -81,7 +81,7 @@ std::string compileStableHLOToTTIR(std::string_view code) {
   }
   mlir::tt::ttir::StableHLOToTTIRPipelineOptions shlo_options;
   shlo_options.arithDialectConversionsEnabled = true;
-  shlo_options.removeDeadValuesEnabled = true;
+  shlo_options.removeDeadValuesEnabled = false;
   shlo_options.legalizeCompositeToCallEnabled = true;
   mlir::tt::ttir::createStableHLOToTTIRPipeline(shlo_pm, shlo_options);
   // Run the pass manager.
