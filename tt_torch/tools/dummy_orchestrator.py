899c3711173b118112e7874b00becf5b2c563dce
James Zian Xu <jameszianxu@tenstorrent.com>
2025-03-11 15:19:53 +0000

    Add execution & cleanup for tracy capture

--- tt_torch/tools/dummy_orchestrator.py
+++ tt_torch/tools/dummy_orchestrator.py
@@ -1,4 +1,15 @@
 # SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
-# test
+
+# run as
+# python3 tt_torch/tools/dummy_orchestrator.py
+import perf
+
+
+def __main__():
+    cvar = perf.Perf()
+    cvar.setup_tracy_server()
+
+
+__main__()
