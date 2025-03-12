14c9d1fd5bcbb0fd3db389e394d5dd2bc59e3370
James Zian Xu <jameszianxu@tenstorrent.com>
2025-03-12 14:59:30 +0000

    dealing with subprocess snarl; ops times writeout

--- tt_torch/tools/dummy_orchestrator.py
+++ tt_torch/tools/dummy_orchestrator.py
@@ -11,10 +11,14 @@ import pytest
 def main():
     cvar = perf.Perf()
     cvar.setup_tracy_server()
+
+    print("Triggering test")
     pytest.main(
         ["-svv", "tests/models/mnist/test_mnist.py::test_mnist_train[full-eval]"]
-    )
+    )  # synchronous and blocking
     # pytest.main(["-svv", "tests/models/mnist/test_mnist.py::test_mnist_train[op_by_op-eval]"])
+
+    cvar.close_capture_tool_process()
     cvar.process_csvexport()
 
 
