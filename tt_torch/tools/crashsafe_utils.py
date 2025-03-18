91a74072306634c240e255329f170264e7ca041b
James Zian Xu <jameszianxu@tenstorrent.com>
2025-03-18 15:44:10 +0000

    Try crashsafe json + json to XML regeneration

--- tt_torch/tools/crashsafe_utils.py
+++ tt_torch/tools/crashsafe_utils.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
 crashsafe_suffix = "_crashsafe.json"
 
 from json2xml import json2xml
@@ -7,11 +10,10 @@ import sys
 # Convert a JSON file to XML
 
 
-
-def reconstruct_junitxml_from_crashsafe(junitxml_path:str):
+def reconstruct_junitxml_from_crashsafe(junitxml_path: str):
     # from here, how can the name of the file be determined?
     # assume it's passed in from the runner / eg. using the shared strings
-    
+
     crashsafe_path = f"{junitxml_path}{crashsafe_suffix}"
     junitxml_path = f"{junitxml_path}_regenerated.xml"
     data = readfromjson(crashsafe_path)
