e8d0f27c6580d79875cf0f2ddc977a9008549916
Kyle Mabee <118925087+kmabeeTT@users.noreply.github.com>
2025-04-10 20:28:12 -0400

    Misc Changes: change falcon model_group, add MobileNetV2_onnx suffix, generate_md.py capture errors (#602)
    ### Ticket
    None
    
    ### What's changed
    - Update test_falcon.py to not have model_group=red, that is intended
    for falcon3 which we haven't added yet.
    - Allow generate_md.py to include compile_error in generated json useful
    for converting to ttnn conv2d sweep configs soon.
     - Deduplicate MobileNetV2 tests by applying suffix MobileNetV2_onnx
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- tests/models/MobileNetV2/test_MobileNetV2_onnx.py
+++ tests/models/MobileNetV2/test_MobileNetV2_onnx.py
@@ -45,7 +45,7 @@ class ThisTester(OnnxModelTester):
 )
 @pytest.mark.parametrize("op_by_op", [True, False], ids=["op_by_op", "full"])
 def test_MobileNetV2(record_property, mode, op_by_op):
-    model_name = "MobileNetV2"
+    model_name = "MobileNetV2_onnx"
     cc = CompilerConfig()
     cc.compile_depth = CompileDepth.STABLEHLO
     if op_by_op:
--- tests/models/falcon/test_falcon.py
+++ tests/models/falcon/test_falcon.py
@@ -53,7 +53,6 @@ def test_falcon(record_property, mode, op_by_op):
         relative_atol=0.015,
         compiler_config=cc,
         record_property_handle=record_property,
-        model_group="red",
     )
     results = tester.test_model()
 
