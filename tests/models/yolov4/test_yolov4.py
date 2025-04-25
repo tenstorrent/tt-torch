8993b9aacf2fbd1ed7c71015c4b5d1f011911548
Kyle Mabee <kmabee@tenstorrent.com>
2025-04-25 00:49:01 +0000

    Initial changes to use tt-forge-models, update tests
     - Bunch of models reference code and input/model loaders moved
       into tt-forge-models (YOLOv3, YOLOv4, OFT, VGG19-Unet) and
       also include Bert from HF for illustrative purposes.
     - No functional differences here, just code organization
     - Use tt-forge-models from public github.com:tenstorrent/tt-forge-models
       via ExternalProject_Add placed in third_party/tt_forge_models/

--- tests/models/yolov4/test_yolov4.py
+++ tests/models/yolov4/test_yolov4.py
@@ -3,37 +3,20 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
-import cv2
-import numpy as np
-import urllib.request
 import pytest
 
-from tests.models.yolov4.src.yolov4 import Yolov4
+from third_party.tt_forge_models.yolov4 import YOLOv4Loader
 from tests.utils import ModelTester
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
 
 
-def url_to_image(url):
-    resp = urllib.request.urlopen(url)
-    image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    return image
-
-
 class ThisTester(ModelTester):
     def _load_model(self):
-        model = Yolov4()
-        return model.to(torch.bfloat16)  # .to(torch.bfloat16)
+        model = YOLOv4Loader.load_model(dtype=torch.bfloat16)
+        return model
 
     def _load_inputs(self):
-
-        img = url_to_image("http://images.cocodataset.org/val2017/000000039769.jpg")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-        img = cv2.resize(img, (640, 480))  # Resize to model input size
-        img = img / 255.0  # Normalize to [0,1]
-        img = np.transpose(img, (2, 0, 1))  # HWC to CHW format
-        img = [torch.from_numpy(img).float().unsqueeze(0)]  # Add batch dimension
-        batch_tensor = torch.cat(img, dim=0).to(torch.bfloat16)
+        batch_tensor = YOLOv4Loader.load_inputs(dtype=torch.bfloat16)
         return batch_tensor
 
 
