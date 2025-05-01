e57392779318d324d7695061ed12072647d41451
Aleks Knezevic <aknezevic@tenstorrent.com>
2025-05-01 20:16:42 +0000

    Remove some unnecessary dtype conversions, add support for input types, add optional support for consteval, to be enabled by default on uplift which contains bugfix

--- tt_torch/csrc/tt-mlir-interface.hpp
+++ tt_torch/csrc/tt-mlir-interface.hpp
@@ -13,7 +13,8 @@ std::shared_ptr<void> *Compile(std::string_view code);
 std::string compileStableHLOToTTIR(std::string_view code);
 std::tuple<std::shared_ptr<void> *, std::string>
 compileTTIRToTTNN(std::string_view code,
-                  std::optional<tt::runtime::Device> device = std::nullopt);
+                  std::optional<tt::runtime::Device> device = std::nullopt,
+                  size_t len_activations = 0, size_t len_graph_constants = 0);
 void create_system_desc(
     std::optional<tt::runtime::Device> device = std::nullopt);
 } // namespace tt::torch
