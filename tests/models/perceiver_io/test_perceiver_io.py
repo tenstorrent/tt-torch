41233493cc906a245b720c827d96019689aa8bd1
Aleks Knezevic <aknezevic@tenstorrent.com>
2025-05-02 18:21:40 +0000

    Don't eliminate dead code until after consteval, breaks consteval pass

--- tests/models/perceiver_io/test_perceiver_io.py
+++ tests/models/perceiver_io/test_perceiver_io.py
@@ -26,8 +26,6 @@ class ThisTester(ModelTester):
         encoding = self.tokenizer(text, padding="max_length", return_tensors="pt")
         # mask " missing.". Note that the model performs much better if the masked span starts with a space.
         encoding.input_ids[0, 52:61] = self.tokenizer.mask_token_id
-        ######inputs, input_mask = encoding.input_ids.to(device), encoding.attention_mask.to(device)
-
         arguments = {
             "inputs": encoding.input_ids,
             "attention_mask": encoding.attention_mask,
