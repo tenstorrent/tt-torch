473db24198712462f6c5fe1c5b8435f0be3c3d71
Vladimir Milosevic <vmilosevic@tenstorrent.com>
2025-05-20 10:27:39 +0000

    Improve Docker image handling in CI workflows
    - separate image building from "latest" tag management
    - add "set-latest" job that runs on push to main branch
    - replace latest tag with output from build job
    - remove unnecessary git submodule cleanup step from build process
    
    Closes: #787

--- .github/build-docker-images.sh
+++ .github/build-docker-images.sh
@@ -13,14 +13,9 @@ CI_IMAGE_NAME=ghcr.io/$REPO/tt-torch-ci-ubuntu-22-04
 DOCKER_TAG=$(./.github/get-docker-tag.sh)
 echo "Docker tag: $DOCKER_TAG"
 
-# Are we on main branch
-ON_MAIN=$(git branch --show-current | grep -q main && echo "true" || echo "false")
-
 build_and_push() {
     local image_name=$1
     local dockerfile=$2
-    local on_main=$3
-
 
     if docker manifest inspect $image_name:$DOCKER_TAG > /dev/null; then
         echo "Image $image_name:$DOCKER_TAG already exists"
@@ -30,22 +25,15 @@ build_and_push() {
             --progress=plain \
             --build-arg FROM_TAG=$DOCKER_TAG \
             -t $image_name:$DOCKER_TAG \
-            -t $image_name:latest \
             -f $dockerfile .
 
         echo "Pushing image $image_name:$DOCKER_TAG"
         docker push $image_name:$DOCKER_TAG
-
-        # If we are on main branch also push the latest tag
-        if [ "$on_main" = "true" ]; then
-            echo "Pushing image $image_name:latest"
-            docker push $image_name:latest
-        fi
     fi
 }
 
-build_and_push $BASE_IMAGE_NAME .github/Dockerfile.base $ON_MAIN
-build_and_push $CI_IMAGE_NAME .github/Dockerfile.ci $ON_MAIN
+build_and_push $BASE_IMAGE_NAME .github/Dockerfile.base
+build_and_push $CI_IMAGE_NAME .github/Dockerfile.ci
 
 echo "All images built and pushed successfully"
 echo "CI_IMAGE_NAME:"
