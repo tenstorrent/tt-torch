acb56325257e89791025fb875d8bd75cba63eef7
ssalice <ssalice@tenstorrent.com>
2025-05-20 15:30:11 +0000

    added new files for phi 3, 3.5, 3.5-vision and 4 models. Grouped based on similarity of testing content. Need to modify test files to reflect actual models.

---
+++ tests/models/phi/test_phi_3p5_vision.py
@@ -0,0 +1,87 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+# Phi-3-mini: https://huggingface.co/microsoft/phi-3-mini
+# Phi-3.5-MoE: https://huggingface.co/microsoft/phi-3.5-MoE
+# Phi-3.5-vision: https://huggingface.co/microsoft/phi-3.5-vision
+
+import torch
+import pytest
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from tests.utils import ModelTester, skip_full_eval_test
+from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
+
+
+class ThisTester(ModelTester):
+    def _load_model(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name, torch_dtype=torch.bfloat16
+        )
+        model = AutoModelForCausalLM.from_pretrained(self.model_name)
+        return model.generate
+
+    def _load_inputs(self):
+        input_str = '''def print_prime(n):
+                        """
+                        Print all primes between 1 and n
+                        """'''
+        self.test_input = input_str
+        inputs = self.tokenizer(
+            input_str, return_tensors="pt", return_attention_mask=False
+        )
+        return inputs
+
+
+@pytest.mark.parametrize(
+    "mode",
+    ["eval"],
+)
+@pytest.mark.parametrize(
+    "model_name", ["microsoft/phi-3-mini", "microsoft/phi-3.5-MoE", "microsoft/phi-3.5-vision"]
+)
+@pytest.mark.parametrize(
+    "op_by_op",
+    [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
+    ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
+)
+def test_phi(record_property, model_name, mode, op_by_op):
+    model_group = "red"
+    cc = CompilerConfig()
+    if op_by_op:
+        cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
+        if op_by_op == OpByOpBackend.STABLEHLO:
+            cc.op_by_op_backend = OpByOpBackend.STABLEHLO
+
+    skip_full_eval_test(
+        record_property,
+        cc,
+        model_name,
+        bringup_status="FAILED_RUNTIME",
+        reason="Cannot get the device from a tensor without an allocated buffer - https://github.com/tenstorrent/tt-torch/issues/733",
+        model_group=model_group,
+        model_name_filter="microsoft/phi-2",
+    )
+
+    tester = ThisTester(
+        model_name,
+        mode,
+        compiler_config=cc,
+        record_property_handle=record_property,
+        is_token_output=True,
+        model_group=model_group,
+    )
+
+    # TODO - Enable checking - https://github.com/tenstorrent/tt-torch/issues/528
+    results = tester.test_model(assert_eval_token_mismatch=False)
+
+    if mode == "eval":
+        decoded_output = tester.tokenizer.decode(results[0])
+        print(
+            f"""
+        model_name: {model_name}
+        input: {tester.test_input}
+        output: {decoded_output}
+        """
+        )
+    tester.finalize()
