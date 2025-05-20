9b2873770947ad98f6c07a8cb5d3410bc239938e
ssalice <ssalice@tenstorrent.com>
2025-05-20 18:22:28 +0000

    worked on phi-model 3 file.

--- tests/models/phi/test_phi_3_3p5.py
+++ tests/models/phi/test_phi_3_3p5.py
@@ -2,33 +2,39 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # Phi-3-mini-128k-instruct: https://huggingface.co/microsoft/Phi-3-mini-128k-instruct
-# Phi-3-mini-4k-instruct: https://huggingface.co/microsoft/Phi-3-mini-128k-instruct
-# Phi-3.5-MoE: https://huggingface.co/microsoft/phi-3.5-MoE
+# Phi-3-mini-4k-instruct: https://huggingface.co/microsoft/Phi-3-mini-4k-instruct
+# Phi-3.5-MoE-instruct: https://huggingface.co/microsoft/Phi-3.5-MoE-instruct
 
 import torch
 import pytest
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from tests.utils import ModelTester, skip_full_eval_test
+from tests.utils import ModelTester
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
 
 
 class ThisTester(ModelTester):
     def _load_model(self):
         self.tokenizer = AutoTokenizer.from_pretrained(
-            self.model_name, torch_dtype=torch.bfloat16
+            self.model_name, torch_dtype=torch.auto
         )
         model = AutoModelForCausalLM.from_pretrained(self.model_name)
         return model.generate
 
     def _load_inputs(self):
-        input_str = '''def print_prime(n):
-                        """
-                        Print all primes between 1 and n
-                        """'''
-        self.test_input = input_str
-        inputs = self.tokenizer(
-            input_str, return_tensors="pt", return_attention_mask=False
+        messages = [ 
+            {"role": "system", "content": "You are a helpful AI assistant."}, 
+            {"role": "user", "content": "Can you provide ways to eat combinations of bananas and dragonfruits?"}, 
+            {"role": "assistant", "content": "Sure! Here are some ways to eat bananas and dragonfruits together: 1. Banana and dragonfruit smoothie: Blend bananas and dragonfruits together with some milk and honey. 2. Banana and dragonfruit salad: Mix sliced bananas and dragonfruits together with some lemon juice and honey."}, 
+            {"role": "user", "content": "What about solving an 2x + 3 = 7 equation?"}, 
+        ] 
+        self.test_input_messages = messages
+        inputs = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=True,
+            add_generation_prompt=True, 
+            return_tensors="pt",
+            # return_attention_mask=False
         )
         return inputs
 
@@ -38,7 +44,7 @@ class ThisTester(ModelTester):
     ["eval"],
 )
 @pytest.mark.parametrize(
-    "model_name", ["microsoft/phi-3-mini", "microsoft/phi-3.5-MoE", "microsoft/phi-3.5-vision"]
+    "model_name", ["microsoft/Phi-3-mini-128k-instruct", "microsoft/Phi-3-mini-4k-instruct", "microsoft/Phi-3.5-MoE"]
 )
 @pytest.mark.parametrize(
     "op_by_op",
@@ -46,22 +52,13 @@ class ThisTester(ModelTester):
     ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
 )
 def test_phi(record_property, model_name, mode, op_by_op):
+    # same pytest command for reference: pytest tests/models/phi/test_phi_1_1p5_2.py::test_phi[full-microsoft/Phi-3-mini-128k-instruct-eval]
     model_group = "red"
     cc = CompilerConfig()
     if op_by_op:
         cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
         if op_by_op == OpByOpBackend.STABLEHLO:
             cc.op_by_op_backend = OpByOpBackend.STABLEHLO
-
-    skip_full_eval_test(
-        record_property,
-        cc,
-        model_name,
-        bringup_status="FAILED_RUNTIME",
-        reason="Cannot get the device from a tensor without an allocated buffer - https://github.com/tenstorrent/tt-torch/issues/733",
-        model_group=model_group,
-        model_name_filter="microsoft/phi-2",
-    )
 
     tester = ThisTester(
         model_name,
@@ -80,7 +77,7 @@ def test_phi(record_property, model_name, mode, op_by_op):
         print(
             f"""
         model_name: {model_name}
-        input: {tester.test_input}
+        input: {tester.test_input_messages}
         output: {decoded_output}
         """
         )
