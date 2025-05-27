8de9d8bef11012eae55e768b7673567c884b26be
James Zian Xu <jameszianxu@tenstorrent.com>
2025-05-27 19:59:42 +0000

    Fix premature sigkill of crashsafe testcase

--- tests/torch/tools/test_benchmark.py
+++ tests/torch/tools/test_benchmark.py
@@ -164,7 +164,9 @@ def test_crashsafe_multitest_assert():
 
 
 def try_sigkill_safe_logging(test_command):
-    search_string = "SiliconDriver"  # We know that we passed the constructor when we see this loguru printout: 2025-03-26 20:04:47.944 | INFO     | SiliconDriver   - Opened PCI device 6; KMD version: 1.31.0, IOMMU: disabled
+
+    # We know that we passed model constructor when we see [MODEL NAME] in the output.
+    search_string = "[MODEL NAME]"
 
     process = subprocess.Popen(
         test_command,
