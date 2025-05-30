ac0048f57771b103f7ce7dae4e80a8037396eb17
James Zian Xu <jameszianxu@tenstorrent.com>
2025-05-30 15:26:30 +0000

    Add semi-working generate loop with a lot of issues
    - static cache doesn't get updated or apparently output from the model
    - no input padding means the decoder loop needs to be recompiled for every iteration. input padding messes up the cache positions which is force-overridden to match the input length -- unclear precedent to make this work
    - generate loop does "work", but requires ~8 hidden layers to start producing meaningful output
    -- it also doesn't properly use the static cache, so these outputs are invalid anyways

--- tests/models/llama/test_llama_3b_generate.py
+++ tests/models/llama/test_llama_3b_generate.py
@@ -13,7 +13,7 @@ from transformers import (
 )
 
 
-class ThisTester(ModelTester):
+class PrefillTester(ModelTester):
     def _load_model(self):
 
         # note - use_cache in llamaconfig is set to default to true
@@ -34,6 +34,7 @@ class ThisTester(ModelTester):
     def _load_inputs(self):
         self.test_input = "This is a sample text from "
 
+        input_length = 32
         inputs = self.tokenizer.encode_plus(
             self.test_input,
             return_tensors="pt",
@@ -41,6 +42,8 @@ class ThisTester(ModelTester):
             # padding="max_length",
             truncation=True,
         )
+
+        # max pad + cache_position side effects cause a different graph to be synthesized...
 
         # setup static cache
         batch_size = 1
@@ -54,10 +57,10 @@ class ThisTester(ModelTester):
         )
 
         attention_mask = inputs.attention_mask
-        cache_position = attention_mask.cumsum(dim=-1) - 1
-        cache_position = cache_position.masked_fill(attention_mask == 0, 0)
+        # cache_position = attention_mask.cumsum(dim=-1) - 1
+        # cache_position = cache_position.masked_fill(attention_mask == 0, 0)
 
-        # cache_position=torch.arange(0, input_length)
+        cache_position = torch.arange(0, inputs.input_ids.shape[1])
 
         args = {
             "input_ids": inputs.input_ids,
@@ -65,11 +68,33 @@ class ThisTester(ModelTester):
             "use_cache": True,
             "cache_position": cache_position,
             "attention_mask": attention_mask,
+            "position_ids": cache_position.unsqueeze(0),  # Assuming batch size of 1
         }
         return args
 
-    def set_model_eval(self, model):
+    @torch.inference_mode()
+    def get_torchcompiled_gm(self):
+        model = self.get_framework_model()
+        golden = self.get_golden_outputs(model, self.inputs)
+
+        model = self.compile_model(model, self.compiler_config)
+
         return model
+
+        # outputs = self.run_model(model, self.inputs)
+        # self.record_property("achieved_compile_depth", "EXECUTE")
+
+        # if self.compiler_config._enable_intermediate_verification:
+        #     self.verify_intermediates_after_execution()
+
+        # self._verify_full_execution_output(outputs, golden, assert_eval_token_mismatch)
+        # return outputs
+
+    @torch.inference_mode()
+    def run_model_with_inputs(self, model, inputs):
+        # Run the model with the provided inputs
+        outputs = model(**inputs)
+        return outputs
 
 
 def test_llama_3b(record_property):
@@ -79,7 +104,7 @@ def test_llama_3b(record_property):
     cc.consteval_parameters = True
     mode = "eval"
     model_name = "meta-llama/Llama-3.2-3B"
-    tester = ThisTester(
+    tester = PrefillTester(
         model_name,
         mode,
         compiler_config=cc,
@@ -88,5 +113,55 @@ def test_llama_3b(record_property):
         record_property_handle=record_property,
     )
 
-    results = tester.test_model()
-    tester.finalize()
+    input_args = tester._load_inputs()
+
+    input_ids = input_args["input_ids"]
+    static_cache = input_args["past_key_values"]
+    cache_position = input_args["cache_position"]
+    attention_mask = input_args["attention_mask"]
+
+    # compile prefill fx graph to flatbuffer and run
+
+    max_new_tokens = 5
+    gm = tester.get_torchcompiled_gm()
+
+    generated_ids = input_ids
+    for i in range(max_new_tokens):
+        print("\n===== Decode step", i, "=====\n")
+        print(f"Input args to step {i}", input_args)
+        outputs = tester.run_model_with_inputs(gm, input_args)
+        next_token_ids = outputs.logits[:, -1:].argmax(dim=-1)
+        print(f"Next token id prediction for decode step {i}: {next_token_ids.item()}")
+
+        #  = outputs.past_key_values
+        # -> are past_key_values updated in place?
+        # eg. do I need to update the static_cache object with new past_key_values?
+
+        generated_ids = torch.cat([generated_ids, next_token_ids], dim=-1)
+        print(
+            "\033[91mDecoded output so far: ",
+            tester.tokenizer.decode(generated_ids[0].tolist()),
+            "\033[0m",
+        )
+
+        attention_mask = input_args["attention_mask"]
+        attention_mask = torch.cat(
+            [attention_mask, attention_mask.new_ones((attention_mask.shape[0], 1))],
+            dim=-1,
+        )
+        cache_position = cache_position[-1:] + 1
+
+        # inspect static_cache
+        key_cache0_head0_vals = torch.mean(static_cache.key_cache_0[0, 0, :, :], dim=-1)
+        print("Key cache slice along seqlen: ", key_cache0_head0_vals.tolist())
+
+        input_args = {
+            "input_ids": generated_ids[:, -1:],
+            "past_key_values": static_cache,  # assume in place update for now
+            "use_cache": True,
+            "cache_position": cache_position,
+            "position_ids": cache_position.unsqueeze(0),  # Assuming batch size of 1
+            "attention_mask": attention_mask,
+        }
+
+    print("Generated ids: ", generated_ids)
