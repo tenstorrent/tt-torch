54cd5b8c2ed15d3f6d06ec7ebebfb19db3563301
gengelageTT <gengelage@tenstorrent.com>
2025-06-03 20:35:38 +0000

    additional n300 nightly tests

---
+++ tests/models/deit/test_deit_n300.py
@@ -0,0 +1,92 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+# Reference: https://huggingface.co/facebook/deit-base-patch16-224
+
+from transformers import AutoFeatureExtractor, ViTForImageClassification
+from PIL import Image
+import requests
+import torch
+import pytest
+from tests.utils import ModelTester
+from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
+
+
+class ThisTester(ModelTester):
+    def _load_model(self):
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained(self.model_name)
+        model = ViTForImageClassification.from_pretrained(self.model_name)
+        model = model.to(torch.bfloat16)
+        return model
+
+    def _load_inputs(self):
+        url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        image = Image.open(requests.get(url, stream=True).raw)
+        images = [image] * 16  # Create a batch of 16
+        inputs = self.feature_extractor(images=images, return_tensors="pt")
+        return inputs
+
+    def set_inputs_train(self, inputs):
+        inputs["pixel_values"].requires_grad_(True)
+        return inputs
+
+    def append_fake_loss_function(self, outputs):
+        return torch.mean(outputs.logits)
+
+    def get_results_train(self, model, inputs, outputs):
+        return inputs["pixel_values"].grad
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        pytest.param(
+            "train",
+            marks=pytest.mark.compilation_xfail,
+        ),
+        "eval",
+    ],
+)
+@pytest.mark.parametrize("model_name", ["facebook/deit-base-patch16-224"])
+@pytest.mark.parametrize(
+    "op_by_op",
+    [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
+    ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
+)
+def test_deit(record_property, model_name, mode, op_by_op):
+    if mode == "train":
+        pytest.skip()
+
+    cc = CompilerConfig()
+    cc.enable_consteval = True
+    cc.consteval_parameters = True
+    cc.automatic_parallelization = True
+    cc.mesh_shape = [1, 2]
+    cc.dump_debug = True
+    if op_by_op:
+        cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
+        if op_by_op == OpByOpBackend.STABLEHLO:
+            cc.op_by_op_backend = OpByOpBackend.STABLEHLO
+
+    tester = ThisTester(
+        model_name,
+        mode,
+        required_pcc=0.97,
+        relative_atol=0.015,
+        compiler_config=cc,
+        record_property_handle=record_property,
+        assert_pcc=True,
+        assert_atol=False,
+    )
+    results = tester.test_model()
+
+    if mode == "eval":
+        logits = results.logits
+        # model predicts one of the 1000 ImageNet classes
+        predicted_class_indices = logits.argmax(-1)
+        for i, class_idx in enumerate(predicted_class_indices):
+            print(
+                f"Sample {i}: Predicted class: {tester.framework_model.config.id2label[class_idx.item()]}"
+            )
+
+    tester.finalize()
