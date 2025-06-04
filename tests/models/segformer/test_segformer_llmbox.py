94aee6e186f60ffcb3769ae459f8d6d48cd47577
gengelageTT <gengelage@tenstorrent.com>
2025-06-04 14:15:33 +0000

    initial llmbox batch parallel tests

---
+++ tests/models/segformer/test_segformer_llmbox.py
@@ -0,0 +1,83 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+# Reference: https://huggingface.co/nvidia/segformer-b0-finetuned-ade-512-512
+
+from transformers import SegformerImageProcessor, SegformerForSemanticSegmentation
+from PIL import Image
+import requests
+import pytest
+from tests.utils import ModelTester
+import torch
+from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
+
+
+class ThisTester(ModelTester):
+    def _load_model(self):
+        self.processor = SegformerImageProcessor.from_pretrained(
+            "nvidia/segformer-b0-finetuned-ade-512-512"
+        )
+        model = SegformerForSemanticSegmentation.from_pretrained(
+            "nvidia/segformer-b0-finetuned-ade-512-512"
+        )
+        model = model.to(torch.bfloat16)
+        return model
+
+    def _load_inputs(self):
+        url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        image = Image.open(requests.get(url, stream=True).raw)
+        inputs = self.processor(images=([image] * 32), return_tensors="pt")
+        inputs["pixel_values"] = inputs["pixel_values"].to(torch.bfloat16)
+        return inputs
+
+    def set_inputs_train(self, inputs):
+        inputs["pixel_values"] = inputs["pixel_values"].requires_grad_(True)
+        return inputs
+
+    def append_fake_loss_function(self, outputs):
+        return torch.mean(outputs.logits)
+
+    def get_results_train(self, model, inputs, outputs):
+        return inputs["pixel_values"].grad
+
+
+@pytest.mark.parametrize(
+    "mode",
+    ["train", "eval"],
+)
+@pytest.mark.parametrize(
+    "op_by_op",
+    [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
+    ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
+)
+def test_segformer(record_property, mode, op_by_op):
+    if mode == "train":
+        pytest.skip()
+    model_name = "SegFormer"
+
+    cc = CompilerConfig()
+    cc.enable_consteval = True
+    cc.consteval_parameters = True
+    cc.automatic_parallelization = True
+    cc.mesh_shape = [1, 8]
+    cc.dump_info = True
+    if op_by_op:
+        cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
+        if op_by_op == OpByOpBackend.STABLEHLO:
+            cc.op_by_op_backend = OpByOpBackend.STABLEHLO
+
+    tester = ThisTester(
+        model_name,
+        mode,
+        relative_atol=0.01,
+        compiler_config=cc,
+        # TODO - Enable checking - https://github.com/tenstorrent/tt-torch/issues/527
+        assert_atol=False,
+        record_property_handle=record_property,
+        model_group="red",
+    )
+    results = tester.test_model()
+    if mode == "eval":
+        logits = results.logits  # shape (batch_size, num_labels, height/4, width/4)
+
+    tester.finalize()
