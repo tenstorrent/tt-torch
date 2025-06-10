dcf92849900cae76111f5a437dc4a5991e075e80
Sungjoon Shon <sshon@tenstorrent.com>
2025-06-10 13:26:07 +0000

    Re-enable test_clamp with integer range
    This is fixed after uplift.

--- tests/torch/test_basic.py
+++ tests/torch/test_basic.py
@@ -124,7 +124,18 @@ def test_broadcast(input):
     verify_module(Basic(), inputs=[input])
 
 
-def test_clamp():
+def test_clamp_int_bound():
+    class Basic(nn.Module):
+        def __init__(self):
+            super().__init__()
+
+        def forward(self, x):
+            return torch.clamp(x, min=-1, max=1)
+
+    verify_module(Basic(), input_shapes=[(32, 32)], input_range=(-5, 5))
+
+
+def test_clamp_float_bound():
     class Basic(nn.Module):
         def __init__(self):
             super().__init__()
