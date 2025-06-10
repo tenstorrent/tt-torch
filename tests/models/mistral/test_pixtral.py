1562aee0d8662fbd8d1ad830233065780b9c513f
ddilbaz <ddilbaz@tenstorrent.com>
2025-06-10 17:09:57 +0000

    Test fixed pixtral

--- tests/models/mistral/test_pixtral.py
+++ tests/models/mistral/test_pixtral.py
@@ -3,26 +3,55 @@
 # SPDX-License-Identifier: Apache-2.0
 import torch
 import pytest
-from transformers import AutoTokenizer, LlavaForConditionalGeneration
+from transformers import LlavaForConditionalGeneration, AutoProcessor
 from tests.utils import ModelTester, skip_full_eval_test
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
 
 
 class ThisTester(ModelTester):
     def _load_model(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.model_name, padding_side="left", torch_dtype=torch.bfloat16
-        )
-        model = LlavaForConditionalGeneration.from_pretrained(
+        self.processor = AutoProcessor.from_pretrained(self.model_name)
+        self.model = LlavaForConditionalGeneration.from_pretrained(
             self.model_name, torch_dtype=torch.bfloat16
         )
-        return model
+        return self.model
 
     def _load_inputs(self):
         # Set up sample input
-        # Pixtral is recommended to be used with a question and an image, but here we use a text-only input for simplicity.
-        self.test_input = "How often does the letter r occur in Mistral?"
-        inputs = self.tokenizer.encode_plus(self.test_input, return_tensors="pt")
+        # chat = [
+        #     {
+        #         "role": "user",
+        #         "content": [
+        #             {"type": "text", "content": "Can this animal"},
+        #             {"type": "image", "url": "https://picsum.photos/id/237/200/300"},
+        #             {"type": "text", "content": "live here?"},
+        #             {
+        #                 "type": "image",
+        #                 "url": "https://picsum.photos/seed/picsum/200/300",
+        #             },
+        #         ],
+        #     }
+        # ]
+        # inputs = self.processor.apply_chat_template(
+        #     chat,
+        #     add_generation_prompt=True,
+        #     tokenize=True,
+        #     return_dict=True,
+        #     return_tensors="pt",
+        # )
+
+        # The proper way of handling the inputs is to use the processor. However, starting transformers >= 4.50,
+        # processor returns a different dict than what the model expects.
+        # The following inputs was generated using the processor from transformers 4.47
+        inputs = {
+            "input_ids": torch.tensor(
+                [[1, 3, 12483, 1593, 11386, 10, 51883, 3226, 1063, 10, 4]],
+                dtype=torch.long,
+            ),
+            "attention_mask": torch.tensor(
+                [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], dtype=torch.long
+            ),
+        }
         return inputs
 
 
