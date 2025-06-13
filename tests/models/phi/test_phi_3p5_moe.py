3aada1530c47f25bdab79f7c99b33a8f2a068179
ddilbaz <ddilbaz@tenstorrent.com>
2025-06-13 16:29:16 +0000

    Edit phi 3.5 MoE to use model instead of pipeline

--- tests/models/phi/test_phi_3p5_moe.py
+++ tests/models/phi/test_phi_3p5_moe.py
@@ -13,33 +13,40 @@ from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
 
 class ThisTester(ModelTester):
     def _load_model(self):
-
+        model_dict = dict(model_info_list)
+        model_path = model_dict[self.model_name]
         self.tokenizer = AutoTokenizer.from_pretrained(
-            self.model_name, torch_dtype=torch.bfloat16
+            model_path, torch_dtype=torch.bfloat16
         )
 
+        if self.tokenizer.pad_token_id is None:
+            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+
         self.model = AutoModelForCausalLM.from_pretrained(
-            self.model_name,
+            model_path,
             return_dict=True,
             torch_dtype=torch.bfloat16,
         )
-        pipe = pipeline(
-            "text-generation",
-            model=self.model,
-            tokenizer=self.tokenizer,
-            torch_dtype=torch.bfloat16,
-            pad_token_id=self.tokenizer.eos_token_id,
-        )
-        return pipe
+        self.model.eval()
+        return self.model
 
     def _load_inputs(self):
         self.prompt = """
         Write a short story about a cat:
         """
+        inputs = self.tokenizer(
+            self.prompt,
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=self.tokenizer.model_max_length,
+        )
         arguments = {
-            "text_inputs": self.prompt,
+            "input_ids": inputs.input_ids,
+            "attention_mask": inputs.attention_mask,
             "max_new_tokens": 120,
             "do_sample": True,
+            "pad_token_id": self.tokenizer.pad_token_id,
         }
         return arguments
 
@@ -63,9 +70,10 @@ model_info_list = [
     [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
     ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
 )
-def test_phi(record_property, model_info, mode, op_by_op):
+def test_phi_3p5_moe(record_property, model_info, mode, op_by_op):
     model_group = "red"
-    __, model_name = model_info
+    model_name, model_path = model_info
+
     cc = CompilerConfig()
     cc.enable_consteval = True
     # consteval_parameters is disabled because it results in a memory related crash
@@ -80,6 +88,7 @@ def test_phi(record_property, model_info, mode, op_by_op):
         cc,
         model_name,
         bringup_status="FAILED_FE_COMPILATION",
+        model_group=model_group,
         reason="failed to legalize operation 'stablehlo.reduce'",
     )
 
@@ -89,12 +98,13 @@ def test_phi(record_property, model_info, mode, op_by_op):
         compiler_config=cc,
         record_property_handle=record_property,
         model_group=model_group,
+        run_generate=True,
     )
 
-    results = tester.test_model()
+    results = tester.test_model(assert_eval_token_mismatch=False)
 
     if mode == "eval":
-        decoded_output = results[0]["generated_text"]
+        decoded_output = tester.tokenizer.decode(results[0])
         print(
             f"""
         model_name: {model_name}
