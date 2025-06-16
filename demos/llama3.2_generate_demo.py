17abe9f9783c535181469ab591e99a7c556dd396
James Zian Xu <jameszianxu@tenstorrent.com>
2025-06-16 20:59:54 +0000

    Add generate demo

---
+++ demos/llama3.2_generate_demo.py
@@ -0,0 +1,103 @@
+import torch
+from tt_torch.tools.utils import CompilerConfig
+from tt_torch.dynamo.backend import backend, BackendOptions
+from tt_torch.tools.device_manager import DeviceManager
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    StaticCache,
+)
+import tt_mlir
+import time
+import argparse
+
+def load_model(model_name = "meta-llama/Llama-3.2-3B"):
+    # set up the model and tokenizer
+    model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            torch_dtype=torch.bfloat16,
+            use_cache=True,
+        )
+    
+    model.config.num_hidden_layers = 8
+
+    tokenizer = AutoTokenizer.from_pretrained(
+            model_name, torch_dtype=torch.bfloat16
+        )
+    tokenizer.pad_token = tokenizer.eos_token
+    return model.eval(), tokenizer
+
+def load_inputs(model, tokenizer, test_input= "This is a sample text from ", max_cache_len=64):
+    batch_size = 1
+    inputs = tokenizer.encode_plus(
+        test_input,
+        return_tensors="pt",
+        truncation=True,
+    )
+    
+    # set up static cache
+    static_cache = StaticCache(
+        config=model.config,
+        max_batch_size=batch_size,
+        max_cache_len=max_cache_len,
+        device=model.device,
+        dtype=model.dtype,
+    )
+
+    cache_position = torch.arange(0, inputs.input_ids.shape[1])
+    args = {
+        "input_ids": inputs.input_ids,
+        "past_key_values": static_cache,
+        "use_cache": True,
+        "cache_position": cache_position,
+    }
+    return args
+
+
+@torch.inference_mode()
+def main():
+    model, tokenizer = load_model()
+    input_args = load_inputs(model, tokenizer)
+    generated_ids = input_args['input_ids']
+    
+    cc = CompilerConfig()
+    cc.enable_consteval = True
+    cc.consteval_parameters = True
+    
+    options = BackendOptions()
+    options.compiler_config = cc
+    options.devices = [None] # defer to Executor to inplace-initialize device
+    
+    compare_golden = False # enable golden comparison - significantly slows down test due to h2d transfer of cache weights
+    compiled_model = torch.compile(model, backend=backend, dynamic=False, options=options)
+    
+    tokens_to_generate = 8
+    for i in range(tokens_to_generate):
+        print("\n===== Decode step", i, "=====\n")
+        print(f"Input args to step {i}", input_args)
+        
+        start_time = time.time()
+        
+        # Execute through backend
+        outputs = compiled_model(**input_args)
+        
+        next_token_ids = outputs.logits[:, -1:].argmax(dim=-1)
+
+        generated_ids = torch.cat([generated_ids, next_token_ids], dim=-1)
+        print(
+            "\033[91mDecoded output so far: ",
+            tokenizer.decode(generated_ids[0].tolist()),
+            "\033[0m",
+        )
+        print("Time elapsed for this step: ", time.time() - start_time)
+        cache_position = input_args['cache_position'][-1:] + 1
+
+        input_args = {
+            "input_ids": next_token_ids.to(dtype=torch.int32), 
+            "past_key_values": input_args['past_key_values'],       # updated in place
+            "cache_position": cache_position,
+            "use_cache": True,
+        }
+    
+if __name__ == "__main__":
+    main()
