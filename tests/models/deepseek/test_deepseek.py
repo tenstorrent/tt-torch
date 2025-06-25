24513c293bbd35e553cc40790d581ca11a17cc12
gengelageTT <gengelage@tenstorrent.com>
2025-06-25 21:00:03 +0000

    more tt-forge-model imports

--- tests/models/deepseek/test_deepseek.py
+++ tests/models/deepseek/test_deepseek.py
@@ -3,65 +3,30 @@
 # SPDX-License-Identifier: Apache-2.0
 import torch
 import pytest
-import os
-from unittest.mock import patch
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-from transformers.dynamic_module_utils import get_imports
 from tests.utils import ModelTester
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
-
-
-def fixed_get_imports(filename: str | os.PathLike) -> list[str]:
-    imports = get_imports(filename)
-    if not torch.cuda.is_available() and "flash_attn" in imports:
-        imports.remove("flash_attn")
-    return imports
+from third_party.tt_forge_models.deepseek.pytorch import ModelLoader
 
 
 class ThisTester(ModelTester):
     def _load_model(self):
-        model = None
-        with patch("transformers.dynamic_module_utils.get_imports", fixed_get_imports):
-            config = AutoConfig.from_pretrained(self.model_name, trust_remote_code=True)
-
-            # Modify config
-            config.num_hidden_layers = 6
-            config.num_attention_heads = 16
-            config.hidden_size = 1024
-            config.num_key_value_heads = 16
-            config.intermediate_size = 1024 * 4
-            config.num_experts_per_tok = 2
-            config.q_lora_rank = 256
-            config.use_flash_attention = False
-
-            model = AutoModelForCausalLM.from_config(
-                config,
-                torch_dtype=torch.bfloat16,
-                attn_implementation="eager",
-                trust_remote_code=True,
-            )
-            return model
+        return ModelLoader.load_model(dtype_override=torch.bfloat16)
 
     def _load_inputs(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.model_name, trust_remote_code=True
-        )
-        self.text = "What is machine learning?"
-        self.inputs = self.tokenizer(self.text, return_tensors="pt")
-        return self.inputs
+        return ModelLoader.load_inputs()
 
 
 @pytest.mark.parametrize(
     "mode",
     ["eval"],
 )
-@pytest.mark.parametrize("model_name", ["deepseek-ai/DeepSeek-V3"])
 @pytest.mark.parametrize(
     "op_by_op",
     [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
     ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
 )
-def test_deepseek(record_property, model_name, mode, op_by_op):
+def test_deepseek(record_property, mode, op_by_op):
+    model_name = "deepseek-ai/DeepSeek-V3"
     cc = CompilerConfig()
     cc.enable_consteval = True
     cc.consteval_parameters = True
