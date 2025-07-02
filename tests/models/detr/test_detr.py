f0475e809da3ffc4eddc8a6281e50c29f2453e60
James Zian Xu <jameszianxu@tenstorrent.com>
2025-07-02 13:53:30 +0000

    Disable detr single device and data parallel tests due to nightly dram oom failure breaking test group reporting.
    I want to #skip-onpr-tests

--- tests/models/detr/test_detr.py
+++ tests/models/detr/test_detr.py
@@ -56,9 +56,9 @@ class ThisTester(ModelTester):
 @pytest.mark.parametrize(
     "data_parallel_mode", [False, True], ids=["single_device", "data_parallel"]
 )
+@pytest.mark.skip(reason="Skipping test_detr due to DRAM OOM; see issue #1024")
 def test_detr(record_property, mode, op_by_op, data_parallel_mode):
     model_name = "DETR"
-
     cc = CompilerConfig()
     cc.enable_consteval = True
     cc.consteval_parameters = True
