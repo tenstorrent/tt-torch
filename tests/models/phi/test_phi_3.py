601cc1213fe9a76c9edafc0ccffb008ea7767f30
Sonnet Salice <ssalice@tenstorrent.com>
2025-07-04 13:28:59 -0400

    Microsoft/phi-4 & phi-3 variants bring up (#1023)
    ### Ticket
    https://github.com/tenstorrent/tt-torch/issues/330
    
    ### Problem description
    Tests for the Microsoft phi models, which are priority models, needed to
    be added.
    
    ### What's changed
    Through test_phi_4.py file we can see that phi-4 runs fully in op-by-op
    and compiles fully down to TTNN_IR as well but does not execute
    currently due to OOM issue. For that I've added a skip_eval_test.
    The same applies to Phi-3-mini-4k-instruct and Phi-3-mini-128k-instruct.
    
    CI Changes:
    - Added phi-4 to op-by-op nightly and full-model-nightly under skip_red
    category.
    - Added the two phi-3 variants to full-model-nightly under skip_red
    category.
    - Added phi-3-mini-4k only to op-by-op-nightly as 4k-instruct and
    128k-instruct only differ in token length and I didn't think it was
    necessary to use resources to run the larger token length variant as it
    takes more time.

---
+++ tests/models/phi/test_phi_3.py
@@ -0,0 +1,101 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+# Phi-3-mini-128k-instruct: https://huggingface.co/microsoft/Phi-3-mini-128k-instruct
+# Phi-3-mini-4k-instruct: https://huggingface.co/microsoft/Phi-3-mini-4k-instruct
+
+import torch
+import pytest
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from tests.utils import ModelTester, skip_full_eval_test
+from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
+
+
+class ThisTester(ModelTester):
+    def _load_model(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name, torch_dtype=torch.bfloat16
+        )
+        model = AutoModelForCausalLM.from_pretrained(self.model_name)
+        # model.config.use_cache = False
+        return model
+
+    def _load_inputs(self):
+        messages = [
+            {"role": "system", "content": "You are a helpful AI assistant."},
+            {
+                "role": "user",
+                "content": "Can you provide ways to eat combinations of bananas and dragonfruits?",
+            },
+        ]
+        self.test_input = messages
+        inputs = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=True,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            return_attention_mask=True,
+        )
+        return inputs
+
+
+@pytest.mark.parametrize(
+    "mode",
+    ["eval"],
+)
+@pytest.mark.parametrize(
+    "model_name",
+    [
+        "microsoft/Phi-3-mini-128k-instruct",
+        "microsoft/Phi-3-mini-4k-instruct",
+    ],
+)
+@pytest.mark.parametrize(
+    "op_by_op",
+    [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
+    ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
+)
+def test_phi_3(record_property, model_name, mode, op_by_op):
+    model_group = "red"
+    cc = CompilerConfig()
+    if op_by_op:
+        cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
+        if op_by_op == OpByOpBackend.STABLEHLO:
+            cc.op_by_op_backend = OpByOpBackend.STABLEHLO
+
+    skip_full_eval_test(
+        record_property,
+        cc,
+        model_name,
+        bringup_status="FAILED_RUNTIME",
+        reason="Out of Memory: Not enough space to allocate 201326592 B DRAM buffer across 12 banks, where each bank needs to store 16777216 B",
+        model_group=model_group,
+        model_name_filter=[
+            "microsoft/Phi-3-mini-128k-instruct",
+            "microsoft/Phi-3-mini-4k-instruct",
+        ],
+    )
+
+    tester = ThisTester(
+        model_name,
+        mode,
+        compiler_config=cc,
+        record_property_handle=record_property,
+        is_token_output=True,
+        model_group=model_group,
+        run_generate=True,
+    )
+
+    results = tester.test_model(assert_eval_token_mismatch=False)
+
+    if mode == "eval":
+        decoded_output = tester.tokenizer.decode(results[0])
+        print(
+            f"""
+        model_name: {model_name}
+        input: {tester.test_input}
+        output: {decoded_output}
+        """
+        )
+    tester.finalize()
---
+++ tests/models/phi/test_phi_4.py
@@ -0,0 +1,89 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+# Phi-4: https://huggingface.co/microsoft/phi-4
+import torch
+import pytest
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from tests.utils import ModelTester, skip_full_eval_test
+from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
+
+
+class ThisTester(ModelTester):
+    def _load_model(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name, torch_dtype=torch.bfloat16
+        )
+        model = AutoModelForCausalLM.from_pretrained(self.model_name)
+        return model
+
+    def _load_inputs(self):
+        messages = [
+            {"role": "system", "content": "You are a helpful AI assistant."},
+            {
+                "role": "user",
+                "content": "Can you provide ways to eat combinations of bananas and dragonfruits?",
+            },
+        ]
+
+        self.test_input = messages
+        inputs = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=True,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            # return_attention_mask=True,
+        )
+        return inputs
+
+
+@pytest.mark.parametrize(
+    "mode",
+    ["eval"],
+)
+@pytest.mark.parametrize("model_name", ["microsoft/phi-4"])
+@pytest.mark.parametrize(
+    "op_by_op",
+    [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
+    ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
+)
+def test_phi_4(record_property, model_name, mode, op_by_op):
+    model_group = "red"
+    cc = CompilerConfig()
+    if op_by_op:
+        cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
+        if op_by_op == OpByOpBackend.STABLEHLO:
+            cc.op_by_op_backend = OpByOpBackend.STABLEHLO
+
+    skip_full_eval_test(
+        record_property,
+        cc,
+        model_name,
+        bringup_status="FAILED_RUNTIME",
+        reason="Out of Memory: Not enough space to allocate 734003200 B DRAM buffer across 12 banks, where each bank needs to store 61169664 B",
+        model_group=model_group,
+    )
+
+    tester = ThisTester(
+        model_name,
+        mode,
+        compiler_config=cc,
+        record_property_handle=record_property,
+        is_token_output=True,
+        model_group=model_group,
+        run_generate=True,
+    )
+
+    results = tester.test_model(assert_eval_token_mismatch=False)
+
+    if mode == "eval":
+        decoded_output = tester.tokenizer.decode(results[0])
+        print(
+            f"""
+        model_name: {model_name}
+        input: {tester.test_input}
+        output: {decoded_output}
+        """
+        )
+    tester.finalize()
