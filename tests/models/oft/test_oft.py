be891496231deddc25d540f4ed0f98b77c0eea9e
Jonathan Azpur <jazpur@tenstorrent.com>
2025-07-09 20:40:32 +0000

    fix test and adding config file for test args

--- tests/models/oft/test_oft.py
+++ tests/models/oft/test_oft.py
@@ -4,7 +4,7 @@
 import pytest
 from tests.utils import ModelTester, skip_full_eval_test
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
-from third_party.tt_forge_models.oft.pytorch import ModelLoader
+from third_party.tt_forge_models.oft.pytorch.loader import ModelLoader
 
 
 class ThisTester(ModelTester):
