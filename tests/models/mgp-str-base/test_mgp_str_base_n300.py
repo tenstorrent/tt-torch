6d15c23698b6af3a5f4f6e928ba4780fd32ad8f0
gengelageTT <gengelage@tenstorrent.com>
2025-07-10 20:37:39 +0000

    update mgp_str_base and mnist

--- tests/models/mgp-str-base/test_mgp_str_base_n300.py
+++ tests/models/mgp-str-base/test_mgp_str_base_n300.py
@@ -10,28 +10,15 @@ from transformers import MgpstrProcessor, MgpstrForSceneTextRecognition
 import pytest
 from tests.utils import ModelTester
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
+from third_party.tt_forge_models.mgp_str_base.pytorch.loader import ModelLoader
 
 
 class ThisTester(ModelTester):
     def _load_model(self):
-        model = MgpstrForSceneTextRecognition.from_pretrained(
-            "alibaba-damo/mgp-str-base", torch_dtype=torch.bfloat16
-        )
-        self.processor = MgpstrProcessor.from_pretrained(
-            "alibaba-damo/mgp-str-base", torch_dtype=torch.bfloat16
-        )
-        return model
+        return self.loader.load_model(dtype_override=torch.bfloat16)
 
     def _load_inputs(self):
-        url = "https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/ocr-demo.png"  # generated_text = "ticket"
-        image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
-        images = [image] * 16  # Create a batch of 16
-        inputs = self.processor(
-            images=images,
-            return_tensors="pt",
-        )
-        inputs["pixel_values"] = inputs["pixel_values"].to(torch.bfloat16)
-        return inputs
+        return self.loader.load_inputs(dtype_override=torch.bfloat16, batch_size=16)
 
 
 @pytest.mark.parametrize(
@@ -62,9 +49,14 @@ def test_mgp_str_base(record_property, mode, op_by_op):
     # TODO Enable checking - https://github.com/tenstorrent/tt-torch/issues/552
     disable_checking = True
 
+    loader = ModelLoader(variant=None)
+    model_info = loader.get_model_info(variant=None)
+
     tester = ThisTester(
-        model_name,
+        model_info.name,
         mode,
+        loader=loader,
+        model_info=model_info,
         relative_atol=0.02,
         compiler_config=cc,
         record_property_handle=record_property,
