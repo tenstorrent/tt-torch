2d6866225cde576e8e1539f8dbac6dbc415bbf4a
Lewis Panos <lpanos@tenstorrent.com>
2025-07-15 17:02:40 +0000

    Fixes and add mm eager and compiled tests

--- tests/experimental/test_torch_xla_basic.py
+++ tests/experimental/test_torch_xla_basic.py
@@ -5,11 +5,41 @@ import tt_torch
 from tt_torch.tools.verify import verify_against_golden
 import torch
 import torch_xla.core.xla_model as xm
+import os
 
 
 def test_simple_mm():
-    # breakpoint()
-    # tt_torch.set_tt_metal_home(device_api="tt-xla") # IMPORTANT
+    os.environ["TT_TORCH_EXPERIMENTAL_USE_XLA"] = "1"
+
+    class MM(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.linear = torch.nn.Linear(32, 64, bias=False)
+
+        def forward(self, x):
+            return self.linear(x)
+
+    device = xm.xla_device()
+    input_x = torch.randn(32, 32)
+
+    model = MM()
+    golden = model(input_x)
+
+    model = torch.compile(model, backend="tt")
+
+    output = model(input_x)
+
+    verify_against_golden(
+        (golden,),
+        (output,),
+        assert_pcc=True,
+        assert_atol=True,
+        required_pcc=0.99,
+        required_atol=0.01,
+    )
+
+
+def test_simple_mm_eager():
     class MM(torch.nn.Module):
         def __init__(self):
             super().__init__()
