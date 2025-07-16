7718d887145a51f28f4e058f5403ee9414a5fecf
Lewis Panos <lpanos@tenstorrent.com>
2025-07-16 15:57:09 +0000

    Restore opt, phi, and torch vision test files

--- tests/models/phi/test_phi_1_1p5_2.py
+++ tests/models/phi/test_phi_1_1p5_2.py
@@ -53,8 +53,6 @@ def test_phi(record_property, model_name, mode, op_by_op):
         if op_by_op == OpByOpBackend.STABLEHLO:
             cc.op_by_op_backend = OpByOpBackend.STABLEHLO
 
-    cc.enable_consteval = True
-
     tester = ThisTester(
         model_name,
         mode,
