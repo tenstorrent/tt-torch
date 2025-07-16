dddd7b62c3e86c4d561c02f0a5c3eebe913a0a18
Lewis Panos <lpanos@tenstorrent.com>
2025-07-16 14:50:00 +0000

    Revert qwen main flow changes, add experimental qwen test

--- tests/experimental/models/Qwen/test_qwen2_casual_lm.py
+++ tests/experimental/models/Qwen/test_qwen2_casual_lm.py
@@ -10,6 +10,12 @@ from tests.utils import ModelTester
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
 import tt_mlir
 from third_party.tt_forge_models.qwen.casual_lm.pytorch import ModelLoader
+import torch_xla.core.xla_model as xm
+from torch.utils._pytree import tree_flatten
+
+from tt_torch.tools.utils import (
+    calculate_pcc,
+)
 
 
 class ThisTester(ModelTester):
@@ -21,22 +27,16 @@ class ThisTester(ModelTester):
 
 
 @pytest.mark.parametrize(
-    "mode",
-    ["eval", "train"],
-)
-@pytest.mark.parametrize(
     "op_by_op",
-    [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
-    ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
+    [OpByOpBackend.TORCH, None],
+    ids=["op_by_op_torch", "full"],
 )
-def test_qwen2_casual_lm(record_property, mode, op_by_op):
-    if mode == "train":
-        pytest.skip()
+def test_qwen2_causal_lm(record_property, op_by_op):
     cc = CompilerConfig()
     if op_by_op:
         cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
-        if op_by_op == OpByOpBackend.STABLEHLO:
-            cc.op_by_op_backend = OpByOpBackend.STABLEHLO
+
+    cc.enable_consteval = True
 
     # TODO: Remove this once PCC ATOL is fixed on blackhole runners - https://github.com/tenstorrent/tt-torch/issues/1003
     assert_pcc = tt_mlir.get_arch() != tt_mlir.Arch.BLACKHOLE
@@ -46,7 +46,7 @@ def test_qwen2_casual_lm(record_property, mode, op_by_op):
 
     tester = ThisTester(
         model_info.name,
-        mode,
+        "eval",
         loader=loader,
         model_info=model_info,
         compiler_config=cc,
@@ -54,16 +54,36 @@ def test_qwen2_casual_lm(record_property, mode, op_by_op):
         assert_pcc=assert_pcc,
         assert_atol=False,
         run_generate=False,
-        required_pcc=0.86,
+        required_pcc=0.85,
+        backend="tt-experimental",
     )
 
     results = tester.test_model()
 
-    if mode == "eval":
-        gen_text = loader.decode_output(results, dtype_override=torch.bfloat16)
+    gen_text = loader.decode_output(results, dtype_override=torch.bfloat16)
 
-        print(
-            f"Model: {model_info.name} | Input: {loader.text} | Decoded Text: {gen_text}"
-        )
+    print(f"Model: {model_info.name} | Input: {loader.text} | Decoded Text: {gen_text}")
 
     tester.finalize()
+
+
+def test_qwen2_causal_lm_eager():
+    loader = ModelLoader(variant=None)
+
+    model = loader.load_model(dtype_override=torch.bfloat16)
+    inputs = loader.load_inputs(dtype_override=torch.bfloat16)
+
+    cpu_outputs = model(**inputs).logits
+
+    device = xm.xla_device()
+    inputs["input_ids"] = inputs["input_ids"].to(device)
+    model = model.to(device)
+
+    tt_outputs = model(**inputs).logits.to("cpu")
+
+    gen_text_cpu = loader.decode_output((cpu_outputs,))
+    gen_text_tt = loader.decode_output((tt_outputs,))
+
+    print(f'CPU Decoded Text: "{gen_text_cpu}"')
+    print(f'TT Decoded Text: "{gen_text_tt}"')
+    print(f"PCC: {calculate_pcc(tt_outputs, cpu_outputs)}")
