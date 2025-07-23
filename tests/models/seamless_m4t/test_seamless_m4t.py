a0fa9443d4e46bdc7a0fc66ced9f7ff5b430aa31
gengelageTT <gengelage@tenstorrent.com>
2025-07-23 22:09:12 +0000

    update 2 more tests

--- tests/models/seamless_m4t/test_seamless_m4t.py
+++ tests/models/seamless_m4t/test_seamless_m4t.py
@@ -2,60 +2,21 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # Reference: https://huggingface.co/facebook/seamless-m4t-v2-large
+
 import torch
-import requests
 import pytest
-import urllib.request
-import io
-import scipy
+
 from tests.utils import ModelTester
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
-
-from transformers import AutoProcessor, SeamlessM4TModel, SeamlessM4TConfig
-from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
-from transformers.integrations.fsdp import is_fsdp_managed_module
-from transformers.modeling_outputs import BaseModelOutput
-import torchaudio
+from third_party.tt_forge_models.seamless_m4t.pytorch import ModelLoader
 
 
 class ThisTester(ModelTester):
     def _load_model(self):
-        model_name = "facebook/hf-seamless-m4t-large"
-        self.config = SeamlessM4TConfig.from_pretrained(model_name)
-        self.processor = AutoProcessor.from_pretrained(model_name)
-        self.full_model = SeamlessM4TModel.from_pretrained(
-            model_name, config=self.config
-        )
-        # Compile text_decoder submodule only
-        return self.full_model.text_decoder
+        return self.loader.load_model()
 
     def _load_inputs(self):
-        # Load and format audio input
-        url = "https://courses.cs.duke.edu/cps001/spring06/class/06_Sound/sounds/preamble.wav"
-        with urllib.request.urlopen(url) as response:
-            audio_data = response.read()
-        audio_buffer = io.BytesIO(audio_data)
-        audio, orig_freq = torchaudio.load(audio_buffer)
-        audio = torchaudio.functional.resample(
-            audio, orig_freq=orig_freq, new_freq=16_000
-        )
-        audio_inputs = self.processor(audios=audio, return_tensors="pt")
-        # Run encoder to get encoder_hidden_states
-        encoder_outputs = self.full_model.speech_encoder(
-            input_features=audio_inputs.input_features,
-            attention_mask=audio_inputs.attention_mask,
-        )
-        encoder_hidden_states = encoder_outputs[0]
-        # Prepare decoder input IDs
-        tokenizer = self.processor.tokenizer
-        bos_token_id = tokenizer.bos_token_id
-        decoder_input_ids = torch.tensor([[bos_token_id]])
-        # Arguments are inputs for the text decoder submodule
-        arguments = {
-            "input_ids": decoder_input_ids,
-            "encoder_hidden_states": encoder_hidden_states,
-        }
-        return arguments
+        return self.loader.load_inputs()
 
 
 @pytest.mark.parametrize(
@@ -68,7 +29,11 @@ class ThisTester(ModelTester):
     ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
 )
 def test_seamless_m4t(record_property, mode, op_by_op):
-    model_name = "SeamlessM4T"
+    loader = ModelLoader(variant=None)
+    model_info = loader.get_model_info(variant=None)
+    model_name = model_info.name
+    model_group = model_info.group.value
+
     cc = CompilerConfig()
     cc.enable_consteval = True
     # cc.consteval_parameters = True
@@ -80,14 +45,27 @@ def test_seamless_m4t(record_property, mode, op_by_op):
     tester = ThisTester(
         model_name,
         mode,
+        loader=loader,
         compiler_config=cc,
         record_property_handle=record_property,
         assert_atol=False,
         run_generate=False,
-        model_group="red",
+        model_group=model_group,
     )
+
     results = tester.test_model()
+
     if mode == "eval":
+        # Use loader's decode_output method
+        decoded_output = loader.decode_output(results)
+
+        print(
+            f"""
+        model_name: {model_name}
+        {decoded_output}
+        """
+        )
+
         if tester.run_generate:
             sample_rate = tester.model.config.sampling_rate
             # uncomment this to download the output audio
