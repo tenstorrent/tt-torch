f29f9472971826c4b229eb9b2aa6597690f92607
James Xu <jameszianxu@tenstorrent.com>
2025-07-24 19:07:20 +0000

    Add consteval to qwen, codegen, llama3, mistral, opt, phi; remove device management from distilbert multiloop

--- tests/models/phi/test_phi_1_1p5_2.py
+++ tests/models/phi/test_phi_1_1p5_2.py
@@ -48,6 +48,8 @@ class ThisTester(ModelTester):
 def test_phi(record_property, model_name, mode, op_by_op):
     model_group = "red"
     cc = CompilerConfig()
+    cc.enable_consteval = True
+    cc.consteval_parameters = True
     if op_by_op:
         cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
         if op_by_op == OpByOpBackend.STABLEHLO:
