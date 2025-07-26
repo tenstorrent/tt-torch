dd34e3238fa72171b50940ab99bf0420e162d0ec
Kyle Mabee <kmabee@tenstorrent.com>
2025-07-26 19:11:14 +0000

    Remove cc.arg_type_map_override=True from mobilnet/ssd tests causes failure #skip-onpr-tests

--- tests/models/mobilenet_ssd/test_mobilenet_ssd.py
+++ tests/models/mobilenet_ssd/test_mobilenet_ssd.py
@@ -52,7 +52,6 @@ def test_mobilenet_ssd(record_property, mode, op_by_op, data_parallel_mode):
     cc = CompilerConfig()
     cc.enable_consteval = True
     cc.consteval_parameters = True
-    # cc.arg_type_map_override = True
 
     if op_by_op:
         if data_parallel_mode:
