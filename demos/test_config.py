4fc10f78ff1d4de0cae798f85c850e0acb5d9006
Kyle Mabee <kmabee@tenstorrent.com>
2025-07-30 01:27:09 +0000

    Make config a dict of dicts organized by test name

--- demos/test_config.py
+++ demos/test_config.py
@@ -3,13 +3,30 @@
 # SPDX-License-Identifier: Apache-2.0
 # test_config.py
 
-expected_passing = {
-    "test_add[1-2-3]",
-    "test_add[3-3-6]",
-    "test_add[5-3-6]",
-}
-
-known_failures = {
-    "test_add[2-2-5]",
-    "test_add[4-5-10]",
+test_config = {
+    "test_add[1-2-3]": {
+        "status": "expected_passing",
+        "batch_size": 8,
+        "pcc": 0.98,
+    },
+    "test_add[3-3-6]": {
+        "status": "expected_passing",
+        "batch_size": 16,
+        "pcc": 0.97,
+    },
+    "test_add[5-3-6]": {
+        "status": "expected_passing",
+        "batch_size": 4,
+        "pcc": 0.95,
+    },
+    "test_add[2-2-5]": {
+        "status": "known_failure",
+        "batch_size": 2,
+        "pcc": 0.90,
+    },
+    "test_add[4-5-10]": {
+        "status": "known_failure",
+        "batch_size": 1,
+        "pcc": 0.88,
+    },
 }
