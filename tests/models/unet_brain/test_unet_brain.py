9a959bf974a95eae4c7a659b9d7b0dd95375b154
ssalice <ssalice@tenstorrent.com>
2025-07-31 02:34:46 +0000

    modified unet_brain to work with tt-forge-models.

--- tests/models/unet_brain/test_unet_brain.py
+++ tests/models/unet_brain/test_unet_brain.py
@@ -1,56 +1,21 @@
 # SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
-import urllib
+# Reference: https://github.com/mateuszbuda/brain-segmentation-pytorch
+
 import torch
-import numpy as np
-from PIL import Image
-from torchvision import transforms
 import pytest
 from tests.utils import ModelTester
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
+from third_party.tt_forge_models.unet_brain.pytorch import ModelLoader
 
 
 class ThisTester(ModelTester):
     def _load_model(self):
-        """
-        Test UNet for brain MRI segmentation
-        The model is from https://pytorch.org/hub/mateuszbuda_brain-segmentation-pytorch_unet/
-        """
-        torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
-        model = torch.hub.load(
-            "mateuszbuda/brain-segmentation-pytorch",
-            "unet",
-            in_channels=3,
-            out_channels=1,
-            init_features=32,
-            pretrained=True,
-        )
-        model = model.to(torch.bfloat16)
-        return model
+        return self.loader.load_model()
 
     def _load_inputs(self):
-        url, filename = (
-            "https://github.com/mateuszbuda/brain-segmentation-pytorch/raw/master/assets/TCGA_CS_4944.png",
-            "TCGA_CS_4944.png",
-        )
-        try:
-            urllib.URLopener().retrieve(url, filename)
-        except:
-            urllib.request.urlretrieve(url, filename)
-
-        input_image = Image.open(filename)
-        m, s = np.mean(input_image, axis=(0, 1)), np.std(input_image, axis=(0, 1))
-        preprocess = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize(mean=m, std=s),
-            ]
-        )
-        input_tensor = preprocess(input_image)
-        input_batch = input_tensor.unsqueeze(0)
-        input_batch = input_batch.to(torch.bfloat16)
-        return input_batch
+        return self.loader.load_inputs()
 
 
 @pytest.mark.parametrize(
@@ -65,7 +30,9 @@ class ThisTester(ModelTester):
 def test_unet_brain(record_property, mode, op_by_op):
     if mode == "train":
         pytest.skip()
-    model_name = "Unet-brain"
+
+    loader = ModelLoader(variant=None)
+    model_info = loader.get_model_info(variant=None)
 
     cc = CompilerConfig()
     cc.enable_consteval = True
@@ -76,7 +43,12 @@ def test_unet_brain(record_property, mode, op_by_op):
             cc.op_by_op_backend = OpByOpBackend.STABLEHLO
 
     tester = ThisTester(
-        model_name, mode, compiler_config=cc, record_property_handle=record_property
+        model_info.name,
+        mode,
+        loader=loader,
+        model_info=model_info,
+        compiler_config=cc,
+        record_property_handle=record_property,
     )
     results = tester.test_model()
     if mode == "eval":
