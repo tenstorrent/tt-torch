32996e35cb959a7ec1c2dd39072cb7db61c4ceb2
ssalice <ssalice@tenstorrent.com>
2025-08-05 03:43:45 +0000

    test_detr_onnx.py works with tt-forge-models.

--- tests/models/detr/test_detr_onnx.py
+++ tests/models/detr/test_detr_onnx.py
@@ -1,16 +1,11 @@
 # SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
-from PIL import Image
 import torch
-import onnx
-import numpy as np
-from torchvision import transforms
 import pytest
 from tests.utils import OnnxModelTester, skip_full_eval_test
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
-from third_party.tt_forge_models.tools.utils import get_file
-from third_party.tt_forge_models.detr.pytorch import ModelLoader
+from third_party.tt_forge_models.detr.onnx import ModelLoader
 
 
 class ThisTester(OnnxModelTester):
@@ -18,37 +13,9 @@ class ThisTester(OnnxModelTester):
         """
         The model is from https://github.com/facebookresearch/detr
         """
-        # Model
-        """torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
-        self.torch_model = torch.hub.load(
-            "facebookresearch/detr:main", "detr_resnet50", pretrained=True
-        )
-        model = self.torch_model.eval()
-
-        # Export to ONNX
-        torch.onnx.export(model, self._load_torch_inputs(), f"{self.model_name}.onnx")
-        model = onnx.load(f"{self.model_name}.onnx")
-        os.remove(f"{self.model_name}.onnx")
-        return model"""
-
-        return self.loader.load_model(dtype_override=torch.bfloat16)
+        return self.loader.load_model()
 
     def _load_torch_inputs(self):
-        # Images
-        """image_file = get_file(
-            "https://huggingface.co/spaces/nakamura196/yolov5-char/resolve/8a166e0aa4c9f62a364dafa7df63f2a33cbb3069/ultralytics/yolov5/data/images/zidane.jpg"
-        )
-        input_image = Image.open(str(image_file))
-        m, s = np.mean(input_image, axis=(0, 1)), np.std(input_image, axis=(0, 1))
-        preprocess = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize(mean=m, std=s),
-            ]
-        )
-        input_tensor = preprocess(input_image)
-        input_batch = input_tensor.unsqueeze(0)
-        return input_batch"""
         return self.loader.load_inputs(dtype_override=torch.bfloat16)
 
     def _extract_outputs(self, output_object):
@@ -65,9 +32,6 @@ class ThisTester(OnnxModelTester):
     ids=["op_by_op_stablehlo", "full"],
 )
 def test_detr_onnx(record_property, mode, op_by_op):
-    model_name = "DETR_onnx"
-    model_group = "red"
-
     cc = CompilerConfig()
     cc.enable_consteval = True
     cc.consteval_parameters = True
@@ -76,14 +40,14 @@ def test_detr_onnx(record_property, mode, op_by_op):
         cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
         cc.op_by_op_backend = op_by_op
 
-    skip_full_eval_test(
+    """skip_full_eval_test(
         record_property,
         cc,
         model_name,
         bringup_status="FAILED_RUNTIME",
         reason="Out of Memory: Not enough space to allocate 59244544 B L1 buffer across 64 banks, where each bank needs to store 925696 B - https://github.com/tenstorrent/tt-torch/issues/729",
         model_group=model_group,
-    )
+    )"""
 
     loader = ModelLoader(variant=None)
     model_info = loader.get_model_info(variant=None)
@@ -97,7 +61,6 @@ def test_detr_onnx(record_property, mode, op_by_op):
         assert_atol=False,
         compiler_config=cc,
         record_property_handle=record_property,
-        model_group=model_group,
     )
     results = tester.test_model()
 
