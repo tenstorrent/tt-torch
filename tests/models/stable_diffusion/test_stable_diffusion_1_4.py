54c6a26dd1a7a8626bfd64c8686cf7b47917fce6
ssalice <ssalice@tenstorrent.com>
2025-08-06 18:51:30 +0000

    Changes for SD for compatibility with forge-models. test_stable_diffusion.py name changed to include 1_4. CI adjusted for stable_diffusion_1_4 and 3_5. test files changed for SD 1_4, 3_5 and unet.

--- tests/models/stable_diffusion/test_stable_diffusion_1_4.py
+++ tests/models/stable_diffusion/test_stable_diffusion_1_4.py
@@ -7,21 +7,17 @@ import torch
 import pytest
 from tests.utils import ModelTester
 from tt_torch.tools.utils import CompilerConfig, CompileDepth, OpByOpBackend
+from third_party.tt_forge_models.stable_diffusion_1_4.pytorch import (
+    ModelLoader,
+)
 
 
 class ThisTester(ModelTester):
     def _load_model(self):
-        model_id = "CompVis/stable-diffusion-v1-4"
-        pipe = StableDiffusionPipeline.from_pretrained(
-            model_id, torch_dtype=torch.bfloat16
-        )
-        return pipe
+        return self.loader.load_model(dtype_override=torch.bfloat16)
 
     def _load_inputs(self):
-        prompt = [
-            "a photo of an astronaut riding a horse on mars",
-        ]
-        return prompt
+        return self.loader.load_inputs(dtype_override=torch.bfloat16)
 
 
 @pytest.mark.parametrize(
@@ -33,7 +29,7 @@ class ThisTester(ModelTester):
     [OpByOpBackend.STABLEHLO, OpByOpBackend.TORCH, None],
     ids=["op_by_op_stablehlo", "op_by_op_torch", "full"],
 )
-def test_stable_diffusion(record_property, mode, op_by_op):
+def test_stable_diffusion_1_4(record_property, mode, op_by_op):
     model_name = "Stable Diffusion"
 
     cc = CompilerConfig()
@@ -43,9 +39,19 @@ def test_stable_diffusion(record_property, mode, op_by_op):
         cc.compile_depth = CompileDepth.EXECUTE_OP_BY_OP
         if op_by_op == OpByOpBackend.STABLEHLO:
             cc.op_by_op_backend = OpByOpBackend.STABLEHLO
+
+    loader = ModelLoader(variant=None)
+    model_info = loader.get_model_info(variant=None)
+
     tester = ThisTester(
-        model_name, mode, compiler_config=cc, record_property_handle=record_property
+        model_info.name,
+        mode,
+        loader=loader,
+        model_info=model_info,
+        compiler_config=cc,
+        record_property_handle=record_property,
     )
+
     results = tester.test_model()
     if mode == "eval":
         image = results.images[0]
