8f49a39eb26f8726eb710a5fbe4c1ede51890b8b
Het Shah <hshah@tenstorrent.com>
2025-08-26 14:04:29 -0400

    Add Eager Tensor Parallel tests to CI (#1202)
    ### Ticket
    https://github.com/tenstorrent/tt-xla/issues/975
    
    ### Problem description
    No TP tests are currently in CI (either here or in tt-xla)
    
    ### What's changed
    Adding CCL Ops tests (for n300 and llmbox) and Llama 3.1 8B eager test
    (for llmbox)
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

--- tests/utils.py
+++ tests/utils.py
@@ -32,6 +32,23 @@ import io
 import csv
 import tt_mlir
 
+# Torch-XLA imports
+import torch_xla.runtime as xr
+from torch_xla.distributed.spmd import Mesh
+
+
+def create_device_mesh(mesh_shape, mesh_names):
+    assert len(mesh_shape) == len(
+        mesh_names
+    ), "Mesh shape and names must match in length"
+    num_devices = xr.global_runtime_device_count()
+    assert (
+        np.prod(mesh_shape) == num_devices
+    ), "Mesh shape must match the number of devices"
+    device_ids = np.array(range(num_devices))
+    mesh = Mesh(device_ids, mesh_shape, mesh_names)
+    return mesh
+
 
 def skip_full_eval_test(
     record_property,
